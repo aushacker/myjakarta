26439c2fe9ec2b5486d0dbfa9834d541c78ff330
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-17 17:07:43 +1100

    Refactor jdbc modules

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -2,9 +2,5 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-echo '*'
-echo '* install.sh'
-echo ${injected_dir}
-echo '*'
-#install_modules ${injected_dir}/modules
+install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
