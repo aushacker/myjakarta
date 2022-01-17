691c7fc6038d9c0686d96c74603f8c5718cac4a4
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-17 16:54:24 +1100

    Add drivers.env

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 injected_dir=$1
-#source /usr/local/s2i/install-common.sh
+source /usr/local/s2i/install-common.sh
 echo '*'
 echo '* install.sh'
 echo ${injected_dir}
 echo '*'
+#install_modules ${injected_dir}/modules
+configure_drivers ${injected_dir}/drivers.env
