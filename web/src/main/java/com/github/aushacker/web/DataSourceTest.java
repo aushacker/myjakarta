ccad86bdd9db7cb75c711e2f152c6005b810378b
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-19 11:55:54 +1100

    Add datasource testing

---
+++ web/src/main/java/com/github/aushacker/web/DataSourceTest.java
@@ -0,0 +1,35 @@
+package com.github.aushacker.web;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+public class DataSourceTest extends HttpServlet {
+
+    @Resource(lookup = "java:/NorthwindDS")
+    private DataSource northwindDS;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        PrintStream out = new PrintStream(resp.getOutputStream());
+
+        try (Connection conn = northwindDS.getConnection())
+        {
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery("SELECT 1");
+            out.println("Connected to database OK");
+        }
+        catch (SQLException e) {
+            e.printStackTrace(out);
+        }
+    }
+}
