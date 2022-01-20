c0e301ac4886ba9792dadb7f161418a9e6799a12
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-20 13:33:14 +1100

    Revert copied modules

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -2,6 +2,7 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-#install_modules ${injected_dir}/modules
-cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql
+#cp -rf ${install_dir}/* $JBOSS_HOME/modules
+install_modules ${injected_dir}/modules
+#cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql
 configure_drivers ${injected_dir}/drivers.env
