c711ade612340be950689769e34cd7242cde5e2a
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-20 13:47:11 +1100

    Improve documentation

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -2,9 +2,9 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-#cp -rf ${install_dir}/* $JBOSS_HOME/modules
-#ls ${injected_dir}/modules
-#install_modules ${injected_dir}/modules
+# Using install_modules ${injected_dir}/modules
+# copies ALL data in the modules dir including licences and README.md.
+# Only using the MySQL driver here.
 mkdir -p $JBOSS_HOME/modules/com/mysql
 cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql
 configure_drivers ${injected_dir}/drivers.env
