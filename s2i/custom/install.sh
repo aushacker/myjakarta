e0c528e56e42e791e2f6634c278668892e4e0a3d
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-20 13:25:52 +1100

    Restrict copied modules during s2i

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -3,5 +3,5 @@
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
 #install_modules ${injected_dir}/modules
-cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql/
+cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql
 configure_drivers ${injected_dir}/drivers.env
