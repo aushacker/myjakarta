0399746c761e753ec2f39667c649bfd5d2db4349
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-20 13:58:50 +1100

    Use external installation script

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -2,9 +2,6 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-# Using install_modules ${injected_dir}/modules
-# copies ALL data in the modules dir including licences and README.md.
-# Only using the MySQL driver here.
-mkdir -p $JBOSS_HOME/modules/com/mysql
-cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql
+source ${injected_dir}/install-common-ext.sh
+install_modules_ext ${injected_dir}/modules "com/mysql"
 configure_drivers ${injected_dir}/drivers.env
