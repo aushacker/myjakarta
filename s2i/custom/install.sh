18625ec7566e03896359f3e1bea10bedda8ff8d0
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-20 13:41:50 +1100

    mkdir before copying modules

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -3,7 +3,8 @@
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
 #cp -rf ${install_dir}/* $JBOSS_HOME/modules
-ls ${injected_dir}/modules
-install_modules ${injected_dir}/modules
-#cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql
+#ls ${injected_dir}/modules
+#install_modules ${injected_dir}/modules
+mkdir -p $JBOSS_HOME/modules/com/mysql
+cp -rf ${injected_dir}/modules/com/mysql/* $JBOSS_HOME/modules/com/mysql
 configure_drivers ${injected_dir}/drivers.env
