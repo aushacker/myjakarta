70a0a4b2fb37259527c64069137a1c377bd0554d
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-20 14:08:24 +1100

    Debug external installation script

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -2,6 +2,7 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
+ls ${injected_dir}/modules
 source ${injected_dir}/modules/install-common-ext.sh
 install_modules_ext ${injected_dir}/modules "com/mysql"
 configure_drivers ${injected_dir}/drivers.env
