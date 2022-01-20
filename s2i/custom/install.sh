09952049250229d13b5bdc7f62ae51305268b9e6
aushacker <25114336+aushacker@users.noreply.github.com>
2022-01-20 14:02:16 +1100

    Use external installation script

--- s2i/custom/install.sh
+++ s2i/custom/install.sh
@@ -2,6 +2,6 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-source ${injected_dir}/install-common-ext.sh
+source ${injected_dir}/modules/install-common-ext.sh
 install_modules_ext ${injected_dir}/modules "com/mysql"
 configure_drivers ${injected_dir}/drivers.env
